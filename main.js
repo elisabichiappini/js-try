892d75cb29f70de5f5dec2323dbb22ac7a7e8404
Elisa Chiappini <elisabichiappini@gmail.com>
2025-10-19 19:03:06 +0200

    feat: logic

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
-const nums = [2, 3, 4, 7, 8, 9];
+const nums = [2, 3, 4, 7, 8, 9, 2, 2, 2];
+const nomi = ['Ida', 'Samantha', 'Marco', 'Simone', ' Mattia', 'Ivo'];
 
 //Dato un array di numeri, restituisci la somma dei loro quadrati.
 const result = nums.map(n => n * n).reduce((acc, curr) => acc + curr, 0);
@@ -22,7 +23,7 @@ console.log(result3);
 function massimo(arr) {
     let max = arr[0];
     for (let num of arr) {
-        if (num > max) max = num 
+        if (num > max) max = num
     }
     return max;
 }
@@ -32,10 +33,42 @@ console.log(massimo(nums));
 //Dato un array, restituisci un nuovo array con gli elementi in ordine inverso.
 const result4 = (arr) => {
     const inverso = [];
-    for(let i = arr.length - 1; i >= 0; i--) {
+    for (let i = arr.length - 1; i >= 0; i--) {
         inverso.push(arr[i]);
     }
     return inverso;
 }
+console.log(result4(nums));
 
-console.log(result4([1,2,3,4]))
+//Dato un array, restituisci la somma solo dei numeri superiori a 5.
+const result5 = (arr) => {
+    const filter = arr.filter((n) => n >= 5).reduce((acc, curr) => acc + curr, 0);
+    return filter;
+}
+console.log(result5(nums));
+
+//Dato un array di stringhe, restituisci solo quelle con più di 3 caratteri.
+const result6 = (arr) => {
+    const filter = arr.filter(n => n.length > 3);
+    return filter;
+}
+console.log(result6(nomi));
+
+//Conta quante volte appare un valore. Data una lista e un valore, conta quante volte compare quel valore.
+const result7 = (arr, val) => {
+    return arr.filter(n => n === val).length;
+}
+console.log(result7(nums, 4));
+
+//somma degli elementi con indice pari
+const result8 = (arr) => {
+    let newArray = [];
+    arr.forEach((element, index) => {
+        if(index % 2 === 0){
+            newArray.push(element);
+        }
+    });
+    return newArray.reduce((acc, curr) => acc + curr, 0); 
+}
+
+console.log(result8(nums));
