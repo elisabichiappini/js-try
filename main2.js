acdc5bbfd6655711161f8e87bfcb2ec7675bedb7
Elisa Chiappini <elisabichiappini@gmail.com>
2025-10-20 15:12:03 +0200

    feat: intermedio js

---
+++ main2.js
@@ -0,0 +1,25 @@
+'use strict';
+const nums = [2, 4, 7, 8, 5];
+//Calcola la media dei cubi dei numeri di un array.
+const result = (arr) => {
+    const cubi = arr.map((n) => n ** 3).reduce((acc, curr) => acc + curr, 0);
+    const media = cubi / arr.length;
+    return media;;
+}
+console.log(result(nums));
+
+//Dato un array, trova la differenza tra somma dei numeri pari e somma dei dispari.
+const result1 = (arr) => {
+    //trova i numeri pari
+    let pari = 0;
+    let dispari = 0;
+    for(let n of arr) {
+        if(n % 2 === 0) {
+            pari += n;
+        }else {
+            dispari += n;
+        }
+    }
+    return pari - dispari;
+}
+console.log(result1(nums))
